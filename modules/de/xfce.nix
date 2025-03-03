c9370ffb4716d06c776eeb4697a692b629da8394
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-03-03 12:22:24 +0100

    feat: Add some more XFCE stuff

--- modules/de/xfce.nix
+++ modules/de/xfce.nix
@@ -4,13 +4,31 @@
 #
 # TODO:
 # - Look into using bspwm with xfce
+# - Put some stuff (I.E ly and pipewire) into a seperate module
+# - Look into using TBSM
+# - Screenshots with grim
 
 { ... }:
 {
   services.displayManager.ly.enable = true;
 
+  # Audio stuff
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
   services.xserver.desktopManager.xfce = {
     enable = true;
-    enableWaylandSession = true;
+    # TODO(Julius): Enable Wayland. The main problem right (2025-03-03) now is
+    #               that xfwm4 is not ported to Wayland. Which means that I do
+    #               not have access to keyboard shortcuts. Which is quite
+    #               important to my workflow. Soooo... :(
+    enableWaylandSession = false;
   };
 }
