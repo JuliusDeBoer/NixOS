9c39177c21464118203144ccebee54ee609a490d
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-05-26 11:02:30 +0200

    refactor: Add yohra grub theme

--- modules/grub.nix
+++ modules/grub.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
@@ -10,10 +10,22 @@
 
   # Theme
   stylix.targets.grub.enable = false;
+
+  # YoRHa theme
   boot.loader.grub.theme = "${
-    (fetchTarball {
-      url = "https://github.com/krypciak/crossgrub/releases/download/1.0.0/crossgrub.tar.gz";
-      sha256 = "0v2dhcqip7sh41lk5rfbacmc13qmpqcfzrjpbwidymmzbs6dwlgp";
-    })
-  }";
+    pkgs.fetchFromGitHub {
+      owner = "OliveThePuffin";
+      repo = "yorha-grub-theme";
+      rev = "4d9cd37baf56c4f5510cc4ff61be278f11077c81";
+      hash = "sha256-XVzYDwJM7Q9DvdF4ZOqayjiYpasUeMhAWWcXtnhJ0WQ=";
+    }
+    }/yorha-1920x1080";
+
+  # CrossCode theme
+  # boot.loader.grub.theme = "${
+  #   (fetchTarball {
+  #     url = "https://github.com/krypciak/crossgrub/releases/download/1.0.0/crossgrub.tar.gz";
+  #     sha256 = "0v2dhcqip7sh41lk5rfbacmc13qmpqcfzrjpbwidymmzbs6dwlgp";
+  #   })
+  # }";
 }
