c695e2997990a64e379ee777efbede04218a9f76
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2025-12-30 17:33:16 +0100

    chore: Remove Proton-GE

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,32 +1,4 @@
 { pkgs, ... }:
-
-let
-  protonGE = pkgs.stdenv.mkDerivation {
-    # TODO(Julius): Figure out how to auto enable proton for all video games
-    name = "proton-ge-custom";
-
-    src = fetchTarball {
-      url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-11/GE-Proton9-11.tar.gz";
-      sha256 = "0ymmzjmpywlr9pd5y326xh8l43wh6vz6yv58qn2wdz4ikr3j0srq";
-    };
-
-    # TODO(Julius): Use the xdg variables for this?
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out
-    '';
-  };
-in
 {
   programs.steam.enable = true;
-
-  home-manager.users.julius =
-    { ... }:
-    {
-      home.packages = [
-        protonGE
-      ];
-
-      home.file.".steam/root/compatibilitytools.d/${protonGE.name}".source = "${protonGE}";
-    };
 }
